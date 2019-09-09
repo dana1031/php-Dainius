e67958a19b2f921c8a8e88f6da879e1ccdb6ee0b
dana1031 <54890998+dana1031@users.noreply.github.com>
2019-09-09 12:31:49 +0300

    uzd2

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<?php
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>As valdau HTML</title>
+    <title> <?php print "As valdau HTML" ?></title>
 </head>
 <body>
-    <h1>Vardas- HTML as ase!</h1>
-    <p> Moku kurti statini turini</p>
-</body>
+    <h1><?php print "Dana - HTML as ase!" . "<br>" ?> </h1>
+    <p><?php print "Today is " . $data= date("Y-m-d") . "<br>"; ?> </p>
+    
 </html>
