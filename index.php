807a145cf994710e49553bf34ed17f0721325f15
dana1031 <54890998+dana1031@users.noreply.github.com>
2019-09-09 11:14:05 +0300

    pirmas  html

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>As valdau HTML</title>
+</head>
+<body>
+    <h1> Vardas- HTML as ase!</h1>
+    <p> Moku kurti statini turini</p>
+</body>
+</html>
