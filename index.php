0db1b8aa3b8990a0307d1065599db4b872066b39
dana1031 <dana1031@gmail.com>
2019-09-11 15:23:48 +0300

    BOMBOS SPROGIMAS

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
-<html> 
+<html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP lydes ir <?php date('Y-m-d', strtotime('+rand(1,10) Years'));?> </title>
+        <title>Bomb img game, and date()</title>
+        <style>
+            .bomb-img {
+                background-image: url(http://pngimg.com/uploads/bomb/bomb_PNG16.png);
+                background-size: cover;
+                width: <?php print date('s') * 2; ?>px;
+                height: <?php print date('s') * 2; ?>px;
+            }
+            .bomb-img-00 {
+                background-image: url(https://i.imgur.com/tNxskR4.jpg); 
+                background-size: cover;
+                width: 100px;
+                height:100px;
+            }   
+        </style>
     </head>
     <body>
-        <h1> Vardas- Galbut turesiu <?php print rand(1, 5) ?> <br>
-            vaiku !<br> 
-            <p> Trumpas nebebus prezidentu:
-                <?php print date('Y-m-d', strtotime('+rand(2,10) Years')); ?>
-            </p>
-    </body>   
-</html> 
+        <div class="bomb-img bomb-img-<?php print date('s'); ?>"></div>
+        <div><?php print date('s'); ?></div>
+    </body>
+</html <
