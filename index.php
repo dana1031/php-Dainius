ce464a549adbbc6c161d995c45d2e766debdbc98
dana1031 <dana1031@gmail.com>
2019-09-17 20:40:53 +0300

    cars (pirkti uz sukauptos pinigus)

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
+$skaicius_1 = rand(0,3);
+$skaicius_2 = rand(0,5);
 
-$grikai       = 5000;
-$per_day = rand(200,500);
-
-$grikai_mod = $grikai;
-for ($i = 1; $grikai_mod >= 0; $i++ ) {
- $grikai_mod -= rand(200,500);
-}
-
-$data = date('Y-m-d', strtotime('+'. $i .'days' ));
-$text = "Isgyvensiu dar $i dienu , iki $data ";
+for ( $i = 0; $i <= $skaicius_1; $i++){
+    
+    print "skaicius_1 $skaicius_1";
+    print " i=  $i <br>";
+    for ($j = 0; $j <= $skaicius_2; $j++) {
+        
+         print "skaicius_2 =  $skaicius_2 ";
+         print "j = $j <br>";
+    }
+}  
 ?>
 <html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1>Kiek dienu galiu valgyti grikius?</h1>
-        <h2>Rasta grikiu: <?php print $grikai_mod; ?>g.</h2>
-        <h3><?php print $text; ?></h3>
-    </body>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h3></h3>
+</body>
 </html>
