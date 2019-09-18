b8fb2dc9dd9c6b425074e4aa799ebe42d5085e15
dana1031 <dana1031@gmail.com>
2019-09-18 22:38:57 +0300

    houmvork foreach $draugo_atmintis

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php
 
-$array =[];
-
-for ($i = 0; $i < 7; $i++) {
-    $day = date('l', strtotime("+$i days"));
-    if ($day == 'Saturday' || $day == 'Sunday') {
-        $array[$day] = 'Weekday';
-    } else {
-        $array[$day] = 'Workday';
-    }             
-}
-
-var_dump($array);
+$mano_atmint = ['vasara', 'jura', 'dziaudsmas', 'saule',
+               'kalnai', 'geles', 'smelis', 'vejas'
+];
+$draugo_atmint = ['snegas', 'kalnai', 'slides', 'rogutes',
+                  'ledas', 'vejas', 'saule', 'dziaugsmas'
+];
 
 ?>
+
+<html>
+    <head>
+        <meta charset ="UTF-8">
+        <title>Uzduotis #3</title>
+    </head>
+    <body>
+        <h2>Draugo atmintis</h2>
+        <ul>
+            <?php foreach ($draugo_atmint as $prisiminimai): ?>
+            <li><?php print $prisiminimai;?></li>
+            <?php endforeach;?>
+        </ul>
+    </body>    
+</html>
+
+
