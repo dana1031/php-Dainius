15fc69d793ec49c835cc93b6b76ca517f3f9dadb
dana1031 <dana1031@gmail.com>
2019-09-19 12:24:58 +0300

    foreach bank-class

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <?php
 
-$mano_atmint = ['vasara', 'jura', 'dziaudsmas', 'saule',
-               'kalnai', 'geles', 'smelis', 'vejas'
-];
-$draugo_atmint = ['snegas', 'kalnai', 'slides', 'rogutes',
-                  'ledas', 'vejas', 'saule', 'dziaugsmas'
+$bank_report = [
+    [
+        'name' => 'iki darbo uzmokestis',
+        'amount' => 600,
+    ],
+    [
+        'name' => 'Kalvariju Nacnykas',
+        'amount' => -15,
+    ],
+    [
+        'name' => 'Geliu turgus',
+        'amount' => 12,
+    ]
 ];
 
+foreach ($bank_report as $key => $value) {
+
+    if ($value['amount'] > 0) {
+        $bank_report[$key]['css_class'] = 'income';
+    } else {
+
+        $bank_report[$key]['css_class'] = 'expence';
+    }
+}
+var_dump($bank_report);
 ?>
-
-<html>
-    <head>
-        <meta charset ="UTF-8">
-        <title>Uzduotis #3</title>
-    </head>
-    <body>
-        <h2>Draugo atmintis</h2>
-        <ul>
-            <?php foreach ($draugo_atmint as $prisiminimai): ?>
-            <li><?php print $prisiminimai;?></li>
-            <?php endforeach;?>
-        </ul>
-    </body>    
-</html>
 
 
