c2b42d9a58f790b6c754a355ba0e9a5afc4ec5cf
dana1031 <dana1031@gmail.com>
2019-09-20 00:02:14 +0300

    ka moteris turi rankinuke

--- index.php
+++ index.php
@@ -1,36 +1,29 @@
 <?php
 
-$array =[
-    'monday'    => 'workday',
-    'tuesday'   => 'workday',
-    'wednesday' => 'workday',
-    'thursday'  => 'workday',
-    'friday'    => 'workday',
-    'saturday'  => 'weekday',
-    'sunday'    => 'weekday'
-];
+$daiktai =['pinigine', 'uzrasu knygele', 'parkeris', 'sukos',' lupu dazai','raktai'];
+    
+$kart =rand(1,5);
+$rankinukas = [];
 
-$tasksArray =[
-    'keliames 6:00',
-    'Sporto klubas',
-    'Rytinis kamstis'.
-    'Code Academy',
-    'Vakarinis kamstis',
-    'Netflix'
-];
-
-foreach ($array as $key => $diena) {
-    if ($diena === 'workday'){    
-        $array[$key] = $tasksArray;
-        
-        if ($key === 'friday'){
-            $tasksArray[5] = 'Baras';
-            $array[$key] = $tasksArray;
-        }
-    }
+for ($i = 0; $i < $kart; $i++) {
+    $index = rand(1,5);
+    $rankinukas[$i]['name'] = $daiktai[$index];
+    $rankinukas[$i]['size'] = rand(1,10);
 }
-foreach ($array as $key => $diena){
-    unset($array[$key]);
-}
-var_dump($array);
-
+var_dump($rankinukas);
+?>
+<html>
+    <head>
+        <meta charset = "UTF-8">   
+        <title>Rankinuke</title>
+    </head>
+    <body>
+        <h1>Ka moteris turi rankinuke</h1>
+        <ul><?php foreach ($rankinukas as $item):?></ul>
+      
+                   <li><?php print $item['name']; ?> uzima
+                        <?php print $item['size']; ?>   cm3
+                    </li>
+                  <?php endforeach; ?>
+    </body>            
+</html>
