64abf94acb91be38e7a4505b727b5df5c42addb5
dana1031 <dana1031@gmail.com>
2019-09-23 21:29:29 +0300

    natos (foreach)

--- index.php
+++ index.php
@@ -1,29 +1,49 @@
 <?php
-
-$daiktai =['pinigine', 'uzrasu knygele', 'parkeris', 'sukos',' lupu dazai','raktai'];
-    
-$kart =rand(1,5);
-$rankinukas = [];
-
-for ($i = 0; $i < $kart; $i++) {
-    $index = rand(1,5);
-    $rankinukas[$i]['name'] = $daiktai[$index];
-    $rankinukas[$i]['size'] = rand(1,10);
+$natos = [
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'A',
+	'B'
+];
+$song = [];
+$natu_suma = count($natos);
+for ($i = 0; $i < 3; $i++) {
+	$root = rand(0, $natu_suma - 1);
+	
+	for ($j = 0; $j < 3; $j++, $root += 2) {
+		if ($root >= $natu_suma) {
+			$note = $root - $natu_suma;
+			$song[$i][$note] = $natos[$note];
+		} else {
+			$note = $root;
+			$song[$i][$note] = $natos[$note];
+		}
+	}
 }
-var_dump($rankinukas);
+var_dump($song);
 ?>
 <html>
-    <head>
-        <meta charset = "UTF-8">   
-        <title>Rankinuke</title>
-    </head>
-    <body>
-        <h1>Ka moteris turi rankinuke</h1>
-        <ul><?php foreach ($rankinukas as $item):?></ul>
-      
-                   <li><?php print $item['name']; ?> uzima
-                        <?php print $item['size']; ?>   cm3
-                    </li>
-                  <?php endforeach; ?>
-    </body>            
+<head>
+	<title>Song</title>
+	<style>
+		.key {
+			height: 200px;
+			width: 50px;
+			border: 1px solid black;
+			display: inline-block;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<?php foreach ($natos as $nata): ?>
+			<div class="key">
+				<?php print $nata; ?>
+			</div>
+		<?php endforeach; ?>
+	</div>
+</body>
 </html>
