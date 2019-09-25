8301e1b257d650786e07f88ca13750f7ae4e082e
dana1031 <dana1031@gmail.com>
2019-09-25 13:32:40 +0300

    sizedd

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 
 $array = [];
-function slot_run($size) {
- for ($i=0; ($i <= $size); $i++) {
-     for($j = 0; ($j <= $size); $j++) {
-     $array[$i][$j] = rand(0,1);      
-  }
-   $array[$i][$j] = rand(0,$size);
- }
-  return ($array); 
-}
-$size = rand(0,5);
-$array = slot_run($size);
-var_dump($array); 
 
+function slot_run($size) {
+    for ($i = 0; ($i <= $size); $i++) {
+        for ($j = 0; ($j <= $size); $j++) {
+            $array[$i][$j] = rand(0, 1);
+        }
+    }
+
+    return $array;
+}
+
+$array = slot_run(3);
+var_dump($array);
 ?>
 
 
-  
- 
+
