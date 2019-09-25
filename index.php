e2e54ff3d2f9aea9ee0c9b9e1796934235bec904
dana1031 <dana1031@gmail.com>
2019-09-25 11:08:27 +0300

    function is_prime

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
-$x =4;
-$y =5;
-function sum($x,$y){
-    $sum = $x + $y;
-    return $sum;
-}   
-$sum = sum($x,$y);
+$x = rand(1, 20);
 
+function is_prime($x) {
+
+    if ($x == 1) {
+        return false;
+    }
+    for ($i = 2; ($i <= $x / 2); $i++) {
+        if ($x % $i == 0) {
+            return false;
+        }
+        return true;
+    }
+}
+
+if (is_prime($x) == 'true') {
+    $text = $x . ' yra pirminis skaicius';
+} else {
+    $text = $x . ' nera pirminis skaicius';
+}
 ?>
 
 <html>
     <body>
         <h1>
-            <?php print $x;?>ir<?php print $y; ?>suma:<?php print $sum;?>
+            <?php print $text; ?>
         </h1>
-    </body>
-        
+    </body>    
 </html> 
