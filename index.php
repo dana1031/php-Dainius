fe4aedd9e2ed4e9153884598d70858f67d9538d0
dana1031 <dana1031@gmail.com>
2019-09-27 23:08:36 +0300

    recursija

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
 <?php
-
-//Turime masyvą:$array = ['w','t','r','r','t'];
-//Parašyti f-iją
-//change_values($array, $val_from, $val_to)
-//kuri visus elementus lygius
-//$val_from pakeistų į $val_to
-//
-//F-ija turi veikti reference‘ų pagalba, be return‘o.
-
-$array = ['w','t','r','r','t'];
-
-function  change_values(&$array, $val_from, $val_to) {
-  foreach ($array as &$raide) {
-    if ($raide === $val_from) {
-        $raide = $val_to;
-    }    
-  }
+$array = [
+	'value1',
+	[
+		'deep1',
+		[
+			'deep2'
+		]
+	],
+	'stringas',
+	'stringas2',
+	[
+		'stringas3',
+		[
+			'stringas4'
+		]
+	]
+];
+function array_return($array) {
+	$string = '';
+	
+	foreach ($array as $value) {
+		if (substr($string, -1) === '.') {
+			$string = substr_replace($string, ', ', -1);
+		}
+		
+		if (!is_array($value)) {
+			$string .= "$value.";
+		} else {
+			$string .= array_return($value);
+		}
+	}
+	return $string;
 }
-$backup = $array;
-
-change_values($array, 't','T');
-var_dump($array);
-var_dump($backup);
+var_dump(array_return($array)); 
