a11c242e1a1a3bc71c0bded45842a1ac42d946c8
dana1031 <dana1031@gmail.com>
2019-09-30 21:31:37 +0300

    forma anketa-sertificat

--- index.php
+++ index.php
@@ -1,41 +1,58 @@
 <?php
-var_dump($_POST);
-$value = 0;
-$img = "https://media.istockphoto.com/photos/banana-picture-id636739634?k=6&m=636739634&s=612x612&w=0&h=BQ9Z6DobjFzclh3LN7nKSljrRqycJPCq65CS8rtUHU4=";
-
-if (isset($_POST['enter'])) {
-    $value = $_POST['enter'];
-    $value++;
+$anketa = [];
+if (isset($_POST['submit'])) {
+    $anketa['vardas'] = 'Vardas: ' . $_POST['vardas'];
+    $anketa['pavarde'] = 'Pavardė: ' . $_POST['pavarde'];
+    $anketa['amzius'] = 'Amžius: ' . $_POST['amzius'];
+    $anketa['lygis'] = 'Lygis: ' . $_POST['lygis'];
+	$anketa['tekstas'] = $_POST['vardas'] . ', kuriam yra ' . $_POST['amzius'] . ' metai, atsidūrė anketoje ir jis yra ' . lcfirst($_POST['lygis']);
 }
 ?>
-
 <html>
-    <head>    
-        <meta charset="UTF-8">
-        <title> forms </title>
+<head>
+	<meta charset="UTF-8">
+	<title>Forms</title>
+	<link rel="stylesheet" href="../includes/normalize.css">
+	<link rel="stylesheet" href="../includes/style.css">
+</head>
+<body>
+	<?php if (empty($_POST)): ?>
+		<div class="pildymas">
+		   <h1>Užpildyk formą:</h1>
+		   <form method="post">
+			   <label>
+				   <span>Vardas:</span>
+				   <input type="text" name="vardas" placeholder="Vardas" required>
+			   </label>
+			   <label>
+				   <span>Pavardė:</span>
+				   <input type="text" name="pavarde" placeholder="Pavardė" required>
+			   </label>
+			   <label>
+				   <span>Amžius:</span>
+				   <input type="number" name="amzius" min="0" max="100" placeholder="Amžius">
+			   </label>
+			   <label for="lygis">
+				   <span>Kaip vertini savo PHP žinias?</span>
+			   </label>
+			   <select name="lygis">
+				   <option value="Pradedantysis" selected>Pradedantysis</option>
+				   <option value="Pažengęs">Pažengęs</option>
+				   <option value="Profesionalas">Profesionalas</option>
+			   </select>
+			   <input type="submit" name="submit" value="Pateikti">
+		   </form>
+		</div>
 
-        <style>
-/*            .bananas{
-                background-image: url("https://media.istockphoto.com/photos/banana-picture-id636739634?k=6&m=636739634&s=612x612&w=0&h=BQ9Z6DobjFzclh3LN7nKSljrRqycJPCq65CS8rtUHU4=");
-                background-size: cover;
-                background-repeat: no-repeat;
-                display: inline-block;
-                width: 150px;
-                height: 150px;
-                
-            }*/
-            img {
-              width: 50px;  
-            } 
-
-        </style>
-    </head>
-    <body>
-        <form  action ="index.php" method = "POST">
-            <span>Bananų skaicius:</span>    
-            <input name = "enter"   value = "<?php print $value; ?>" type = "submit"> 
-        </form> 
-        <?php for ($i = 1; $i <= $value; $i++): ?>
-            <img src="https://media.istockphoto.com/photos/banana-picture-id636739634?k=6&m=636739634&s=612x612&w=0&h=BQ9Z6DobjFzclh3LN7nKSljrRqycJPCq65CS8rtUHU4="  alt="bananan">
-        <?php endfor; ?>
-    </body>       
+	<!--Sertifikatas-->
+	<?php else: ?>
+		<div class="anketa-bg">
+			<img src="https://img.pngio.com/free-png-quality-control-certified-stamp-png-images-transparent-certified-stamp-png-850_655.png" width="120 px">
+			<p class="pav">ANKETA</p>
+			<?php foreach ($anketa as $irasas): ?>
+				<p class="anketa"><?php print $irasas; ?></p>
+			<?php endforeach; ?>
+		</div>
+	<?php endif; ?>
+</body>
+</html> 
