b355af1f78ed1bd4d451d6dc8ca9798b69d7d5ca
dana1031 <dana1031@gmail.com>
2019-10-03 15:03:50 +0300

    forms FIELDS

--- index.php
+++ index.php
@@ -1,95 +1,108 @@
 <?php
 $form = [
-	'attr' => [
-		'action' => 'index.php',
-		'class' => 'bg-black'
-	],
-	'title' => 'Kalėdų norai',
-	'fields' => [
-		'first_name' => [
-			'attr' => [
-				'type' => 'text'
-			],
-			'extra' => [
-				'attr' => [
-					'placeholder' => 'Aurimas',
-					'class' => 'input-text',
-					'id' => 'first-name'
-				]
-			],
-			'label' => 'Vardas:',
-			'error' => 'Vardas per trumpas!'
-		],
-		'last_name' => [
-			'attr' => [
-				'type' => 'text'
-			],
-			'extra' => [
-				'attr' => [
-					'placeholder' => 'Stecenka',
-					'class' => 'input-text',
-					'id' => 'last-name'
-				]
-			],
-			'label' => 'Pavardė:',
-			'error' => 'Paliktas tuščias laukas!'
-		],
-		'wish' => [
-			'attr' => [
-				'type' => 'select',
-				'value' => 'tv'
-			],
-			'extra' => [
-				'attr' => [
-					'class' => 'input-select',
-					'id' => 'wish'
-				]
-			],
-			'options' => [
-				'car' => 'BMW',
-				'tv' => 'Teliko',
-				'socks' => 'Kojinių'
-			],
-			'label' => 'Kalėdom noriu:',
-		]
-	],
-	'buttons' => [
-		'submit' => [
-			'type' => 'submit',
-			'value' => 'Siųsti'
-		],
-		'reset' => [
-			'type' => 'reset',
-			'value' => 'Išvalyti'
-		]
-	],
-	'message' => 'Formos Message!'
+    'attr' => [
+        'action' => 'index.php',
+        'class' => 'bg-black'
+    ],
+    'title' => 'Kalėdų norai',
+    'fields' => [
+        'first_name' => [
+            'attr' => [
+                'type' => 'text'
+            ],
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Aurimas',
+                    'class' => 'input-text',
+                    'id' => 'first-name'
+                ]
+            ],
+            'label' => 'Vardas:',
+            'error' => 'Vardas per trumpas!'
+        ],
+        'last_name' => [
+            'attr' => [
+                'type' => 'text'
+            ],
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Stecenka',
+                    'class' => 'input-text',
+                    'id' => 'last-name'
+                ]
+            ],
+            'label' => 'Pavardė:',
+            'error' => 'Paliktas tuščias laukas!'
+        ],
+        'wish' => [
+            'attr' => [
+                'type' => 'select',
+                'value' => 'tv'
+            ],
+            'extra' => [
+                'attr' => [
+                    'class' => 'input-select',
+                    'id' => 'wish'
+                ]
+            ],
+            'options' => [
+                'car' => 'BMW',
+                'tv' => 'Teliko',
+                'socks' => 'Kojinių'
+            ],
+            'label' => 'Kalėdom noriu:',
+        ]
+    ],
+    'buttons' => [
+        'submit' => [
+            'type' => 'submit',
+            'value' => 'Siųsti'
+        ],
+        'reset' => [
+            'type' => 'reset',
+            'value' => 'Išvalyti'
+        ]
+    ],
+    'message' => 'Formos Message!'
 ];
+
 /**
  * Generates HTML attributes
  * @param array $attr
  * @return string
  */
 function html_attr($attr) {
-	$html_attr_array = [];
-	
-	foreach ($attr as $attribute_key => $attribute_value) {
-		$html_attr_array[] = strtr('@key="@value"', [
-			'@key' => $attribute_key,
-			'@value' => $attribute_value
-		]);
-	}
-	
-	return implode(' ', $html_attr_array);
+    $html_attr_array = [];
+
+    foreach ($attr as $attribute_key => $attribute_value) {
+        $html_attr_array[] = strtr('@key="@value"', [
+            '@key' => $attribute_key,
+            '@value' => $attribute_value
+        ]);
+    }
+
+    return implode(' ', $html_attr_array);
 }
+
+function get_filtered_input($form) {
+    $filter_parameters = [];
+
+    foreach ($form['fields'] as $field_id => $value) {
+        $filter_parameters[$field_id] = FILTER_SANITIZE_SPECIAL_CHARS;
+    }
+    return filter_input_array(INPUT_POST, $filter_parameters);
+}
+
+$filtered_input = get_filtered_input($form);
+var_dump($filtered_input);
 ?>
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>Form Templates</title>
-	<link rel="stylesheet" href="includes/style.css">
-</head>
-<body>
-	<?php require 'templates/form.tpl.php'; ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Form Templates</title>
+        <link rel="stylesheet" href="includes/style.css">
+    </head>
+    <body>
+        <?php require 'templates/form.tpl.php'; ?>
+    </body>
 </html>
