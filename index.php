1ef295a4c377ff8e6ab76ebd6b899f9ca0a6d0e6
dana1031 <dana1031@gmail.com>
2019-10-03 09:21:08 +0300

    form Aurimas

--- index.php
+++ index.php
@@ -1,79 +1,95 @@
 <?php
 $form = [
-    'attr' => [
-        'action' => 'index.php',
-        'method' => 'POST',
-        'class' => 'my-form',
-        'id' => 'login-form'
-    ],
-    'fields' => [
-        'first_name' => [
-            'attr' => [
-                'type' => 'text'
-            ],
-            'extra' => [
-                'class' => 'blue',
-                'placeholder' => 'Piotr'
-            ],
-            'label' => 'vardas',
-        ],
-        'last name' => [
-            'attr' => [
-                'type' => 'text',
-            ],
-            'extra' => [
-                'class' => 'blue',
-                'placeholder' => 'Piotrowski',
-            ],
-            'label' => 'Pavarde',
-            'error' => 'Klaida!',
-        ],
-        'select' => [
-            'attr' => [
-                'type' => 'select',
-            ],
-            'options' => [
-                'vyras' => 'Vyras',
-                'moteris' => 'Moteris',
-                'vaikas' => 'Vaikas'
-            ],
-        ],
-    ],
-    'buttons' => [
-        'submit' => [
-            'type' => 'submit',
-            'value' => 'send',
-        ],
-        'reset' => [
-            'type' => 'reset',
-            'value' => 'delete'
-        ],
-    ],
-    'massage' => 'pavyko!!!',
+	'attr' => [
+		'action' => 'index.php',
+		'class' => 'bg-black'
+	],
+	'title' => 'Kalėdų norai',
+	'fields' => [
+		'first_name' => [
+			'attr' => [
+				'type' => 'text'
+			],
+			'extra' => [
+				'attr' => [
+					'placeholder' => 'Aurimas',
+					'class' => 'input-text',
+					'id' => 'first-name'
+				]
+			],
+			'label' => 'Vardas:',
+			'error' => 'Vardas per trumpas!'
+		],
+		'last_name' => [
+			'attr' => [
+				'type' => 'text'
+			],
+			'extra' => [
+				'attr' => [
+					'placeholder' => 'Stecenka',
+					'class' => 'input-text',
+					'id' => 'last-name'
+				]
+			],
+			'label' => 'Pavardė:',
+			'error' => 'Paliktas tuščias laukas!'
+		],
+		'wish' => [
+			'attr' => [
+				'type' => 'select',
+				'value' => 'tv'
+			],
+			'extra' => [
+				'attr' => [
+					'class' => 'input-select',
+					'id' => 'wish'
+				]
+			],
+			'options' => [
+				'car' => 'BMW',
+				'tv' => 'Teliko',
+				'socks' => 'Kojinių'
+			],
+			'label' => 'Kalėdom noriu:',
+		]
+	],
+	'buttons' => [
+		'submit' => [
+			'type' => 'submit',
+			'value' => 'Siųsti'
+		],
+		'reset' => [
+			'type' => 'reset',
+			'value' => 'Išvalyti'
+		]
+	],
+	'message' => 'Formos Message!'
 ];
-/* * Generates HTML atributes
+/**
+ * Generates HTML attributes
  * @param array $attr
  * @return string
  */
-
 function html_attr($attr) {
-    $attr_array = [];
-    foreach ($attr as $key => $value) {
-//         $attr_array[] = "$key=" . "$value";
-        $attr_array[] = strtr('@key="@value"', [
-            '@key' => $key,
-            '@value' => $value
-        ]);
-    }
-
-    return implode(" ", $attr_array);
+	$html_attr_array = [];
+	
+	foreach ($attr as $attribute_key => $attribute_value) {
+		$html_attr_array[] = strtr('@key="@value"', [
+			'@key' => $attribute_key,
+			'@value' => $attribute_value
+		]);
+	}
+	
+	return implode(' ', $html_attr_array);
 }
 ?>
 <html>
-    <head>
-
-    </head>
-    <body>
-        <?php require 'templates/form.tpl.php'; ?>
-    </body>
+<head>
+	<meta charset="UTF-8">
+	<title>Form Templates</title>
+	<link rel="stylesheet" href="includes/style.css">
+</head>
+<body>
+	<?php require 'templates/form.tpl.php'; ?>
+</body>
 </html>
