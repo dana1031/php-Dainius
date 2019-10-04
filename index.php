ed25e986d586b7af10c7a4497b54c6d8595a2aa2
dana1031 <dana1031@gmail.com>
2019-10-04 14:12:20 +0300

    forms error

--- index.php
+++ index.php
@@ -1,122 +1,118 @@
+
 <?php
 $form = [
-    'attr' => [
-        'action' => 'index.php',
-        'class' => 'bg-black'
-    ],
-    'title' => 'Kalėdų norai',
-    'fields' => [
-        'first_name' => [
-            'attr' => [
-                'type' => 'text',
-               
-            ],
-            'extra' => [
-                'attr' => [
-                    'placeholder' => 'Aurimas',
-                    'class' => 'input-text',
-                    'id' => 'first-name'
-                ]
-            ],
-            'label' => 'Vardas:',
-            'error' => 'Vardas per trumpas!'
-        ],
-        'last_name' => [
-            'attr' => [
-                'type' => 'text'
-            ],
-            'extra' => [
-                'attr' => [
-                    'placeholder' => 'Stecenka',
-                    'class' => 'input-text',
-                    'id' => 'last-name'
-                ]
-            ],
-            'label' => 'Pavardė:',
-            'error' => 'Paliktas tuščias laukas!'
-        ],
-        'wish' => [
-            'attr' => [
-                'type' => 'select',
-                'value' => 'tv'
-            ],
-            'extra' => [
-                'attr' => [
-                    'class' => 'input-select',
-                    'id' => 'wish'
-                ]
-            ],
-            'options' => [
-                'car' => 'BMW',
-                'tv' => 'Teliko',
-                'socks' => 'Kojinių'
-            ],
-            'label' => 'Kalėdom noriu:',
-        ]
-    ],
-    'buttons' => [
-        'submit' => [
-            'type' => 'submit',
-            'value' => 'Siųsti'
-        ],
-        'reset' => [
-            'type' => 'reset',
-            'value' => 'Išvalyti'
-        ]
-    ],
-    'message' => 'Formos Message!'
+	'attr' => [
+		'action' => 'index.php',
+		'class' => 'bg-black'
+	],
+	'title' => 'Kalėdų norai',
+	'fields' => [
+		'first_name' => [
+			'attr' => [
+				'type' => 'text'
+			],
+			'extra' => [
+				'attr' => [
+					'placeholder' => 'Aurimas',
+					'class' => 'input-text',
+					'id' => 'first-name'
+				]
+			],
+			'label' => 'Vardas:',
+			'error' => 'Vardas per trumpas!'
+		],
+		'last_name' => [
+			'attr' => [
+				'type' => 'text'
+			],
+			'extra' => [
+				'attr' => [
+					'placeholder' => 'Stecenka',
+					'class' => 'input-text',
+					'id' => 'last-name'
+				]
+			],
+			'label' => 'Pavardė:',
+			'error' => 'Paliktas tuščias laukas!'
+		],
+		'wish' => [
+			'attr' => [
+				'type' => 'select',
+				'value' => 'car'
+			],
+			'extra' => [
+				'attr' => [
+					'class' => 'input-select',
+					'id' => 'wish'
+				]
+			],
+			'options' => [
+				'car' => 'BMW',
+				'tv' => 'Teliko',
+				'socks' => 'Kojinių'
+			],
+			'label' => 'Kalėdom noriu:',
+		]
+	],
+	'buttons' => [
+		'submit' => [
+			'type' => 'submit',
+			'value' => 'Siųsti'
+		],
+		'reset' => [
+			'type' => 'reset',
+			'value' => 'Išvalyti'
+		]
+	],
+	'message' => 'Formos Message!'
 ];
 
-/**
- * Generates HTML attributes
- * @param array $attr
- * @return string
- */
 function html_attr($attr) {
-    $html_attr_array = [];
-
-    foreach ($attr as $attribute_key => $attribute_value) {
-        $html_attr_array[] = strtr('@key="@value"', [
-            '@key' => $attribute_key,
-            '@value' => $attribute_value
-        ]);
+	$html_attr_array = [];
+	
+	foreach ($attr as $attribute_key => $attribute_value) {
+		$html_attr_array[] = strtr('@key="@value"', [
+			'@key' => $attribute_key,
+			'@value' => $attribute_value
+		]);
+	}
+	
+	return implode(' ', $html_attr_array);
+}
+function get_form_input($form) {
+	$filter_parameters = [];
+	
+	foreach ($form['fields'] as $field_id => $field) {
+		$filter_parameters[$field_id] = $field['filter'] ?? FILTER_SANITIZE_SPECIAL_CHARS;
+	}
+	
+	return filter_input_array(INPUT_POST, $filter_parameters);
+}
+function validate_not_empty($field_input, &$field) {
+    
+    if ($field_input === ''){
+        $field['error'] = 'Laukas negali būti tuščias!';
     }
-
-    return implode(' ', $html_attr_array);
 }
 
-function get_filtered_input($form) {
-    $filter_parameters = [];
+$filtered_input = get_form_input($form);
 
-    foreach ($form['fields'] as $field_id => $value) {
-        $filter_parameters[$field_id] = FILTER_SANITIZE_SPECIAL_CHARS;
-    }
+foreach ($form['fields'] as $field_id => &$field) {
+    $field_input = $filtered_input[$field_id];
+    $field['attr']['value'] = $field_input;
+    validate_not_empty($field_input, $field);
 
-    return filter_input_array(INPUT_POST, $filter_parameters);
+    unset($field);
 }
 
-$filtered_input = get_filtered_input($form);
-
-
-
-$filter_parameters = [];
-foreach ($form['fields'] as $fields_id => $field) {
-var_dump($fields_id);
-    if ($filtered_input[$fields_id] = ' '){
-        $field['attr']['value'] = 'error ';
-    } else {
-    $field['attr']['value'] = $filtered_input[$fields_id];}
-  //  unset($field);
-    var_dump($filtered_input[$fields_id]);
-}
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Form Templates</title>
-        <link rel="stylesheet" href="includes/style.css">
-    </head>
-    <body>
-<?php require 'templates/form.tpl.php'; ?>
-    </body>
+<head>
+	<meta charset="UTF-8">
+	<title>Form Security</title>
+	<link rel="stylesheet" href="includes/style.css">
+</head>
+<body>
+	<?php require 'templates/form.tpl.php'; ?>
+</body>
 </html>
--- templates/form.tpl.php
+++ templates/form.tpl.php
@@ -1,71 +1,66 @@
-<?php
+<?php if (isset($form) && !empty($form)): ?>
+	<form <?php print html_attr(($form['attr'] ?? []) + ['method' => 'POST']); ?>>
 
+		<!--Form Title-->	
+		<?php if (isset($form['title'])): ?>
+			<h2><?php print $form['title']?></h2>
+		<?php endif; ?>
 
+		<!--Start Field Generation-->
+		<?php foreach ($form['fields'] as $field_id => $field): ?>
+			<div class="field-container">
 
+				<!--Label-->
+				<?php if (isset($field['label'])): ?>
+					<label>
+						<span class="label"><?php print $field['label']; ?></span>
+				<?php endif; ?>
 
-if (isset($form) && !empty($form)): ?>
-    <form <?php print html_attr(($form['attr'] ?? []) + ['method' => 'POST']); ?>>
+				<!--Field-->
+				<?php if ($field['attr']['type'] === 'select'): ?>
+					<select <?php print html_attr(['name' => $field_id] + ($field['extra']['attr'] ?? [])); ?>>
+						<?php foreach ($field['options'] as $option_id => $option): ?>
+							<option value="<?php print $option_id; ?>" <?php print ($field['attr']['value'] ?? null) === $option_id ? 'selected' : ''; ?>>
+								<?php print $option; ?>
+							</option>
+						<?php endforeach; ?>
+					</select>
+				<?php else: ?>
+					<input <?php print html_attr(['name' => $field_id] + $field['attr'] + ($field['extra']['attr'] ?? [])); ?>>
+				<?php endif; ?>
 
-        <!--Form Title-->	
-        <?php if (isset($form['title'])): ?>
-            <h2><?php print $form['title'] ?></h2>
-        <?php endif; ?>
-            
-        <!-<-Start Field Generation-->
-        <?php foreach ($form['fields'] as $field_id => $field): ?>
-            <div class="field-container">
+				<?php if (isset($field['label'])): ?>
+					</label>
+				<?php endif; ?>
 
-                <!--Label-->
-                <?php if (isset($field['label'])): ?>
-                    <label>
-                        <span class="label"><?php print $field['label']; ?></span>
-                    <?php endif; ?>
+				<!--Error-->
+				<div class="error">
+					<?php if (isset($field['error'])): ?>
+						<span>
+							<?php print $field['error']; ?>
+						</span>
+					<?php endif; ?>
+				</div>
 
-                    <!--Field-->
-                    <?php if ($field['attr']['type'] === 'select'): ?>
-                        <select <?php print html_attr(['name' => $field_id] + ($field['extra']['attr'] ?? [])); ?>>
-                            <?php foreach ($field['options'] as $option_id => $option): ?>
-                                <option value="<?php print $option_id; ?>" <?php print ($field['attr']['value'] ?? null) === $option_id ? 'selected' : ''  ?>>
-                                    <?php print $option; ?>
-                                </option>
-                            <?php endforeach; ?>
-                        </select>
-                    <?php else: ?>
-                        <input <?php print html_attr(['name' => $field_id] + $field['attr'] + ($field['extra']['attr'] ?? [])); ?>>
-                    <?php endif; ?>
+			</div>
+		<?php endforeach; ?>
+		<!--End Field Generation-->
 
-                    <?php if (isset($field['label'])): ?>
-                    </label>
-                <?php endif; ?>
+		<!--Start Button Generation-->
+		<?php if (isset($form['buttons'])): ?>
+			<?php foreach ($form['buttons'] as $button_id => $button): ?>
+				<div class="button-container">
+					<input <?php print html_attr(['name' => $button_id] + $button); ?>>
+				</div>
+			<?php endforeach; ?>
+		<?php endif; ?>
+		<!--End button Generation-->
 
-                <!--Error-->
-                <div class="error">
-                    <?php if (isset($field['error'])): ?>
-                        <span>
-                            <?php print $field['error']; ?>
-                        </span>
-                    <?php endif; ?>
-                </div>
-
-            </div>
-        <?php endforeach; ?>
-        <!--End Field Generation-->
-
-        <!--Start Button Generation-->
-        <?php if (isset($form['buttons'])): ?>
-            <?php foreach ($form['buttons'] as $button_id => $button): ?>
-                <div class="button-container">
-                    <input <?php print html_attr(['name' => $button_id] + $button); ?>>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        <!--End button Generation-->
-
-        <!--Form Message-->	
-        <?php if (isset($form['message'])): ?>
-            <div class="message">
-                <span><?php print $form['message'] ?></span>
-            </div>
-        <?php endif; ?>
-    </form>
+		<!--Form Message-->	
+		<?php if (isset($form['message'])): ?>
+			<div class="message">
+				<span><?php print $form['message']?></span>
+			</div>
+		<?php endif; ?>
+	</form>
 <?php endif; ?>
